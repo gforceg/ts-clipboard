98ad0b04309f13a6c22410d1af39d71c62c8c6c0
action jackson <action@localhost.localdomain>
2016-10-31 17:19:12 -0600

    ignore .vscode/ directory incr ver fix copy to clipboard for non-POS browsers.

--- ts-clipboard.ts
+++ ts-clipboard.ts
@@ -1,21 +1,24 @@
 // greg hedin
 
 export class Clipboard {
-  static _data: string = '';
+  static _data: string = null;
   // MSIE boolean
   static POS = !!window.clipboardData;
 
   static init() {
     window.addEventListener('copy', function (e: ClipboardEvent) {
-      e.clipboardData.setData('text/plain', Clipboard._data);
-      // e.preventDefault() // this disables 'ctrl + c' 
+      if (Clipboard._data) {
+        e.clipboardData.setData('text/plain', Clipboard._data);
+        e.preventDefault() // this disables 'ctrl + c'
+        Clipboard._data = null;
+      }
     });
   }
 
   static copy = (data: string): void => {
     Clipboard._data = data;    
     if (!Clipboard.POS) {
-      document.execCommand('copy');
+      document.execCommand('copy')
     } 
     //  POS browsers
     else if (!!window.clipboardData) {
