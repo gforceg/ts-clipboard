436038d0b2b209560cf4b3e5f60b5abe2dbf6593
Ruslan Korkin <rukrak86@gmail.com>
2018-11-21 17:21:06 +0200

    Warning - tslint
    error member-access: The class property '_data' must be marked either 'private', 'public', or 'protected'
    error member-access: The class property 'POS' must be marked either 'private', 'public', or 'protected'
    error member-access: The class method 'init' must be marked either 'private', 'public', or 'protected'
    error member-access: The class property 'copy' must be marked either 'private', 'public', or 'protected'
    error no-function-expression: Use arrow function instead of function expression
    error semicolon: Missing semicolon
    error semicolon: Missing semicolon

--- ts-clipboard.ts
+++ ts-clipboard.ts
@@ -1,25 +1,25 @@
 // greg hedin
 
 export class Clipboard {
-  static _data: string = null;
+  private static _data: string = null;
   // MSIE boolean
-  static POS = !!window['clipboardData'];
-  
-  static init() {
-    window.addEventListener('copy', function (e: ClipboardEvent) {
+  private static POS = !!window['clipboardData'];
+
+  public static init() {
+    window.addEventListener('copy', (e: ClipboardEvent) => {
       if (Clipboard._data) {
         e.clipboardData.setData('text/plain', Clipboard._data);
-        e.preventDefault() // this disables 'ctrl + c'
+        e.preventDefault(); // this disables 'ctrl + c'
         Clipboard._data = null;
       }
     });
   }
 
-  static copy = (data: string): void => {
-    Clipboard._data = data;    
+  public static copy = (data: string): void => {
+    Clipboard._data = data;
     if (!Clipboard.POS) {
-      document.execCommand('copy')
-    } 
+      document.execCommand('copy');
+    }
     //  POS browsers
     else if (!!window['clipboardData']) {
       window['clipboardData'].setData('Text', Clipboard._data);
